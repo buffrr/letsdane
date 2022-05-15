7eea7b1ae19ff7e9f3037cebfec63691e3d5db86
Buffrr <contact@buffrr.dev>
2022-05-15 16:43:47 -0700

    Allow a stricter subset of TLS 1.2 ciphersuites

--- tls.go
+++ tls.go
@@ -24,6 +24,19 @@ func newTLSConfig(host string, rrs []*dns.TLSA, nameCheck bool) *tls.Config {
 		VerifyConnection:   verifyConnection(rrs, nameCheck),
 		ServerName:         host,
 		MinVersion:         tls.VersionTLS12,
+		// Supported TLS 1.2 cipher suites
+		// Crypto package does automatic cipher suite ordering
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+		},
 	}
 }
 
